f6105cd11ba4ca01e130b5b5ee3e7a95855aebaf
LastJonicorn <joni.k.lampela@gmail.com>
2023-03-16 11:39:34 +0200

    Ingredients ja Measures Arrayt ei jostakin syystä toimi

--- components/Cocktails.js
+++ components/Cocktails.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import { Text, View, Pressable, Image, ScrollView, SafeAreaView } from 'react-native';
+import { Text, View, Pressable, Image, ScrollView, SafeAreaView, FlatList } from 'react-native';
 import axios from 'axios';
 import styles from '../style/Style';
 
@@ -28,8 +28,43 @@ export default function About () {
         return <View style={styles.container}><Text style={styles.loading}>Loading...</Text></View>
     }
 
-    console.log(data[0]);
     console.log(data[0].strIngredient1);
+
+/*     const Ingredients = [
+        data[0].strIngredient1,
+        data[0].strIngredient2,
+        data[0].strIngredient3,
+        data[0].strIngredient4,
+        data[0].strIngredient5,
+        data[0].strIngredient6,
+        data[0].strIngredient7,
+        data[0].strIngredient8,
+        data[0].strIngredient9,
+        data[0].strIngredient10,
+        data[0].strIngredient11,
+        data[0].strIngredient12,
+        data[0].strIngredient13,
+        data[0].strIngredient14,
+        data[0].strIngredient15,
+    ];
+
+    const Measures = [
+        data[0].strMeasure1,
+        data[0].strMeasure2,
+        data[0].strMeasure3,
+        data[0].strMeasure4,
+        data[0].strMeasure5,
+        data[0].strMeasure6,
+        data[0].strMeasure7,
+        data[0].strMeasure8,
+        data[0].strMeasure9,
+        data[0].strMeasure10,
+        data[0].strMeasure11,
+        data[0].strMeasure12,
+        data[0].strMeasure13,
+        data[0].strMeasure14,
+        data[0].strMeasure15,
+    ]; */
 
     return (
         <SafeAreaView style={styles.container}>
